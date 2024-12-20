1f26f7011df0605372d006e7c4d4634bd6663e7b
hapoi987 <kckudda@gmail.com>
2024-12-21 05:07:59 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -49,13 +49,13 @@ global.location = process.env.LOCATION || "Pakistan, Lahore, Sabzazar";
 
 
 //~~~~~~~~<Session and Bot Details>~~~~~~~~~~~~~\\
-global.SESSION_ID = process.env.SESSION_ID || '';
+global.SESSION_ID = process.env.SESSION_ID || 'XLICON-V4~GmhE3CRS#CkP3A_E8Sx-hCEhULPNCMSP-yCgDLG2FP0lLhMUdjaU';
 
 global.botname = process.env.BOT_NAME || 'XLICON-V4-MD';
 
-global.ownernumber = [process.env.OWNER_NUMBER || '923184070915'];
+global.ownernumber = [process.env.OWNER_NUMBER || '94771993400'];
 
-global.ownername = process.env.OWNER_NAME || '𝙎𝙖𝙡𝙢𝙖𝙣 𝘼𝙝𝙢𝙖𝙙';
+global.ownername = process.env.OWNER_NAME || 'MR.DINO';
 
 
 //~~~~~~<Website and Social Links>~~~~~~~~~~\\
@@ -81,7 +81,7 @@ global.creator = process.env.CREATOR_NUMBER || "923184070915@s.whatsapp.net";
 //~~~~~~~~~~~~~<Bot Settings>~~~~~~~~~~~~~~~\\
 global.xprefix = process.env.PREFIX || '.';
 
-global.premium = [process.env.PREMIUM_NUMBER || '923184070915'];
+global.premium = [process.env.PREMIUM_NUMBER || '94754633505'];
 
 global.typemenu = process.env.MENU_TYPE || 'v2';
 
